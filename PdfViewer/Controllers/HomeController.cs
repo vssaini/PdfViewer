cd566f6a38b25cc2311b2416b12b61c78ec10503
Vikram Singh Saini <vssaini.dev@gmail.com>
2024-07-13 12:11:45 +0530

    Add project files.

---
+++ PdfViewer/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PdfViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PdfWithBytes()
+        {
+            return View();
+        }
+
+        public ActionResult GetPdf()
+        {
+            string pdfPath = Server.MapPath("~/Content/report.pdf");
+            byte[] pdfBytes = System.IO.File.ReadAllBytes(pdfPath);
+            return File(pdfBytes, "application/pdf");
+        }
+    }
+}
